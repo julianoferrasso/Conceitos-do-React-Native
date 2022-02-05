b3472cc52004db4a7b45c32c64f0d80226a804e7
Juliano <julianoferrasso@gmail.com>
2022-02-05 18:56:58 -0300

    Task Done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -29,7 +29,7 @@ export function Home() {
     } 
 
     setTasks([...updateTasks])
-    //TODO - toggle task done if exists - ok
+    //TODO - toggle task done if exists - ok 
   }
 
   function handleRemoveTask(id: number) {
